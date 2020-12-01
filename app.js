e20c5237a4a30e33dda8bf33f0e7e0354adbdc76
Clems-web <clement.servais1@gmail.com>
2020-12-01 21:21:25 +0100

    Finito !

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let para = document.getElementsByClassName("paragraphe");
 console.log(para);
 
-for (i = 0; i <= para.length; i++) {
+for (i = 0; i < para.length; i++) {
+    if (i === para.length - 1) {
+        break;
+    }
+    if ((i % 2) === 0) {
+        continue;
+    }
     para[i].innerHTML = "paragraphe " + i;
 }
